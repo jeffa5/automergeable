4334cb49e41ae7895ebe1fcee1d32554dd133004
Andrew Jeffery <dev@jeffas.io>
2021-02-23 18:33:00 +0000

    Add template for diff functionality

---
+++ automergeable-core/src/diff.rs
@@ -0,0 +1,3 @@
+pub fn diff(new: automerge::Value, old:automerge::Value) -> Vec<automerge::LocalChange> {
+    Vec::new()
+}
--- automergeable-core/src/lib.rs
+++ automergeable-core/src/lib.rs
@@ -1,4 +1,6 @@
 mod to;
+mod diff;
 
 pub use to::ToAutomerge;
+pub use diff::diff;
 pub trait Automergeable : to::ToAutomerge{}
