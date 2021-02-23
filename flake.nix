efdb3306e5f528fca20ab1eb08032b9c380a9247
Andrew Jeffery <dev@jeffas.io>
2021-02-23 11:16:36 +0000

    Initial setup

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "automergable";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs {
+            overlays = [ rust-overlay.overlay ];
+            system = system;
+          };
+          rust = pkgs.rust-bin.nightly.latest.rust;
+        in
+        rec
+        {
+          devShell = pkgs.mkShell {
+            buildInputs = with pkgs;[
+              (rust.override {
+                extensions = [ "rust-src" "rustfmt" ];
+              })
+              cargo-edit
+              cargo-watch
+              cargo-udeps
+              crate2nix
+
+              rnix-lsp
+              nixpkgs-fmt
+            ];
+          };
+        }
+      );
+}
