9889b2036c720c4ee914206a4a4a9eaac9117b7f
Andrew Jeffery <dev@jeffas.io>
2021-02-24 16:49:55 +0000

    Export FromAutomergeError

--- automergeable/src/lib.rs
+++ automergeable/src/lib.rs
@@ -2,6 +2,6 @@ mod diff;
 mod document;
 
 pub use automergeable_derive::{Automergeable, FromAutomerge, ToAutomerge};
-pub use automergeable_traits::{Automergeable, FromAutomerge, ToAutomerge};
+pub use automergeable_traits::{Automergeable, FromAutomerge, FromAutomergeError, ToAutomerge};
 pub use diff::{diff, diff_values};
 pub use document::Document;
