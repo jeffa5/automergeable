0c864fbecabd39e9c3bf3344c9a60740fcae57c1
Andrew Jeffery <dev@jeffas.io>
2021-02-24 16:49:39 +0000

    Export FromAutomerge

--- automergeable/src/lib.rs
+++ automergeable/src/lib.rs
@@ -1,7 +1,7 @@
 mod diff;
 mod document;
 
-pub use automergeable_derive::{Automergeable, ToAutomerge};
+pub use automergeable_derive::{Automergeable, FromAutomerge, ToAutomerge};
 pub use automergeable_traits::{Automergeable, FromAutomerge, ToAutomerge};
 pub use diff::{diff, diff_values};
 pub use document::Document;
