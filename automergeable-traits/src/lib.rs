2c9712a46cb3424fc8b7e983f00b63e1f38f08b7
Andrew Jeffery <dev@jeffas.io>
2021-02-28 15:01:43 +0000

    Add auto impl for Automergeable

--- automergeable-traits/src/lib.rs
+++ automergeable-traits/src/lib.rs
@@ -6,3 +6,5 @@ pub use to::ToAutomerge;
 
 /// Overall trait for requiring all automerge sub-traits.
 pub trait Automergeable: to::ToAutomerge + from::FromAutomerge {}
+
+impl<T> Automergeable for T where T: to::ToAutomerge + from::FromAutomerge {}
