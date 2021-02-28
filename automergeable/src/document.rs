72e02d9040ddf32a964d6925dc550ff065b1ad56
Andrew Jeffery <dev@jeffas.io>
2021-02-28 18:01:28 +0000

    Add some tests to document

--- automergeable/src/document.rs
+++ automergeable/src/document.rs
@@ -4,8 +4,9 @@ use std::{
     ops::{Deref, DerefMut},
 };
 
-use automerge::Path;
-use automergeable_traits::Automergeable;
+use automerge::{Path, Value};
+
+use crate::Automergeable;
 
 #[derive(Default)]
 pub struct Document<T>
@@ -36,13 +37,19 @@ where
         E: Error,
         F: FnOnce(&mut T) -> Result<(), E>,
     {
-        let original = match self.frontend.get_value(&Path::root()) {
-            Some(value) => T::from_automerge(&value).unwrap(),
-            None => T::default(),
-        };
-        let mut new_t = original.clone();
+        let original = self
+            .frontend
+            .get_value(&Path::root())
+            .map(|value| match &value {
+                Value::Map(hm, automerge::MapType::Map) if !hm.is_empty() => {
+                    Some(T::from_automerge(&value).expect("original from frontend was not valid"))
+                }
+                _ => None,
+            })
+            .flatten();
+        let mut new_t = original.as_ref().cloned().unwrap_or_default();
         change(&mut new_t).unwrap();
-        let changes = crate::diff(&new_t, &original);
+        let changes = crate::diff(&Some(new_t), &original);
         let change =
             self.frontend
                 .change::<_, automerge::InvalidChangeRequest>(message, |doc| {
@@ -95,5 +102,127 @@ where
 {
     fn deref_mut(&mut self) -> &mut Self::Target {
         &mut self.frontend
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn create_from_empty() {
+        #[derive(crate::Automergeable, Debug, Clone, Default)]
+        struct A {
+            list: Vec<String>,
+            others: std::collections::HashMap<String, String>,
+            nah: Option<u64>,
+            yep: Option<i64>,
+            #[automergeable(representation = "Text")]
+            some_text: String,
+            #[automergeable(representation = "Counter")]
+            a_counter: i64,
+            #[automergeable(representation = "Timestamp")]
+            a_timestamp: i64,
+            b: B,
+        }
+
+        #[derive(crate::Automergeable, Debug, Clone, Default)]
+        struct B {
+            inner: u64,
+        }
+        let mut doc: Document<A> = Document::new();
+        let mut back = automerge::Backend::init();
+        let change = doc
+            .change::<_, automerge::InvalidChangeRequest>(|_t| Ok(()))
+            .unwrap();
+        if let Some(change) = change {
+            let (patch, _) = back.apply_local_change(change).unwrap();
+            doc.apply_patch(patch).unwrap();
+        }
+    }
+
+    #[test]
+    fn create_from_empty_then_add_some_fields() {
+        #[derive(crate::Automergeable, Debug, Clone, Default)]
+        struct A {
+            list: Vec<String>,
+            others: std::collections::HashMap<String, String>,
+            nah: Option<u64>,
+            yep: Option<i64>,
+            #[automergeable(representation = "Text")]
+            some_text: String,
+            #[automergeable(representation = "Counter")]
+            a_counter: i64,
+            #[automergeable(representation = "Timestamp")]
+            a_timestamp: i64,
+            b: B,
+        }
+
+        #[derive(crate::Automergeable, Debug, Clone, Default)]
+        struct B {
+            inner: u64,
+        }
+        let mut doc: Document<A> = Document::new();
+        let mut back = automerge::Backend::init();
+        let change = doc
+            .change::<_, automerge::InvalidChangeRequest>(|t| {
+                t.list.push("hi".to_owned());
+                t.others.insert("hellow there".to_owned(), "abc".to_owned());
+                Ok(())
+            })
+            .unwrap();
+        if let Some(change) = change {
+            let (patch, _) = back.apply_local_change(change).unwrap();
+            doc.apply_patch(patch).unwrap();
+        }
+    }
+
+    #[test]
+    fn create_from_empty_then_add_some_fields_later() {
+        #[derive(crate::Automergeable, Debug, Clone, Default)]
+        struct A {
+            list: Vec<String>,
+            others: std::collections::HashMap<String, String>,
+            nah: Option<u64>,
+            yep: Option<i64>,
+            #[automergeable(representation = "Text")]
+            some_text: String,
+            #[automergeable(representation = "Counter")]
+            a_counter: i64,
+            #[automergeable(representation = "Timestamp")]
+            a_timestamp: i64,
+            b: B,
+        }
+
+        #[derive(crate::Automergeable, Debug, Clone, Default)]
+        struct B {
+            inner: u64,
+        }
+        let mut doc: Document<A> = Document::new();
+        let mut back = automerge::Backend::init();
+        let change = doc
+            .change::<_, automerge::InvalidChangeRequest>(|t| {
+                t.list.push("hi".to_owned());
+                t.others.insert("hellow there".to_owned(), "abc".to_owned());
+                Ok(())
+            })
+            .unwrap();
+
+        if let Some(change) = change {
+            let (patch, _) = back.apply_local_change(change).unwrap();
+            doc.apply_patch(patch).unwrap();
+        }
+
+        let change = doc
+            .change::<_, automerge::InvalidChangeRequest>(|t| {
+                t.b.inner += 1;
+                Ok(())
+            })
+            .unwrap();
+
+        if let Some(change) = change {
+            let (patch, _) = back.apply_local_change(change).unwrap();
+            doc.apply_patch(patch).unwrap();
+        }
     }
 }
