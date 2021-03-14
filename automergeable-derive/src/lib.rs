92620eb1058a8ff926c4dc996fb7d46ae7394e02
Andrew Jeffery <dev@jeffas.io>
2021-03-14 14:38:18 +0000

    Update Automergeable comment

--- automergeable-derive/src/lib.rs
+++ automergeable-derive/src/lib.rs
@@ -10,6 +10,7 @@ mod utils;
 ///
 /// Covers:
 /// - conversion into an automerge `Value`
+/// - conversion from an automerge `Value`
 #[proc_macro_derive(Automergeable, attributes(automergeable))]
 pub fn automergeable(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
