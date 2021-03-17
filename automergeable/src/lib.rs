1f81b61366c110c0bb0f4694eecaef46916116d3
Andrew Jeffery <dev@jeffas.io>
2021-03-17 19:16:22 +0000

    Pub use automerge_protocol

--- automergeable/src/lib.rs
+++ automergeable/src/lib.rs
@@ -5,6 +5,7 @@ pub mod traits {
     pub use automergeable_traits::{Automergeable, FromAutomerge, FromAutomergeError, ToAutomerge};
 }
 pub use automerge;
+pub use automerge_protocol;
 pub use automergeable_derive::{Automergeable, FromAutomerge, ToAutomerge};
 pub use diff::diff_values;
 pub use document::Document;
