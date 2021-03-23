742fb664861065daf1e618c7b33c8014ca1351ba
Andrew Jeffery <dev@jeffas.io>
2021-03-23 13:22:13 +0000

    Expose DocumentChangeError

--- automergeable/src/lib.rs
+++ automergeable/src/lib.rs
@@ -8,4 +8,4 @@ pub use automerge;
 pub use automerge_protocol;
 pub use automergeable_derive::{Automergeable, FromAutomerge, ToAutomerge};
 pub use diff::diff_values;
-pub use document::Document;
+pub use document::{Document, DocumentChangeError};
