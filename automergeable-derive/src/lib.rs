a796d240ac2b6c6a8758ed872733c66fdb4fed07
Andrew Jeffery <dev@jeffas.io>
2021-04-19 11:19:14 +0100

    Document derive macros

--- automergeable-derive/src/lib.rs
+++ automergeable-derive/src/lib.rs
@@ -11,6 +11,8 @@ mod utils;
 /// Covers:
 /// - conversion into an automerge `Value`
 /// - conversion from an automerge `Value`
+///
+/// For attributes available, see [`ToAutomerge`] and [`FromAutomerge`] documentation.
 #[proc_macro_derive(Automergeable, attributes(automergeable))]
 pub fn automergeable(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
@@ -24,6 +26,14 @@ pub fn automergeable(input: TokenStream) -> TokenStream {
 }
 
 /// Derive the [`ToAutomerge`](automergeable_traits::ToAutomerge) trait.
+///
+/// # Supported attributes
+///
+/// `#[automerge(representation = "text")]`: Converts a [`String`] to an [`automerge::Value::Text`] type, separating the string into grapheme clusters.
+///
+/// `#[automerge(representation = "counter")]`: Converts an [`i64`] into an [`automerge::Primitive::Counter`] type.
+///
+/// `#[automerge(representation = "timestamp")]`: Converts an [`i64`] into an [`automerge::Primitive::Timestamp`] type.
 #[proc_macro_derive(ToAutomerge, attributes(automergeable))]
 pub fn to_automerge(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
@@ -31,6 +41,14 @@ pub fn to_automerge(input: TokenStream) -> TokenStream {
 }
 
 /// Derive the [`FromAutomerge`](automergeable_traits::FromAutomerge) trait.
+///
+/// # Supported attributes
+///
+/// `#[automerge(representation = "text")]`: Converts an [`automerge::Value::Text`] type to a [`String`], joining grapheme clusters together.
+///
+/// `#[automerge(representation = "counter")]`: Converts an [`automerge::Primitive::Counter`] type to an [`i64`].
+///
+/// `#[automerge(representation = "timestamp")]`: Converts an [`automerge::Primitive::Timestamp`] type to an [`i64`].
 #[proc_macro_derive(FromAutomerge, attributes(automergeable))]
 pub fn from_automerge(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
