c6b14128ad173081582dd5bd556882c976fa010a
Andrew Jeffery <dev@jeffas.io>
2021-05-08 15:55:59 +0100

    Add benchmarks for just to and from conversions

--- automergeable/benches/convert.rs
+++ automergeable/benches/convert.rs
@@ -1,5 +1,6 @@
 use std::collections::HashMap;
 
+use automerge::Value;
 use automergeable::{FromAutomerge, ToAutomerge};
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
@@ -9,6 +10,16 @@ fn bench_hashmap(h: HashMap<String, String>) {
         let ha = h.to_automerge();
         HashMap::<String, String>::from_automerge(&ha).unwrap();
     });
+}
+
+fn bench_hashmap_to(h: HashMap<String, String>) {
+    #[allow(clippy::unit_arg)]
+    black_box(h.to_automerge());
+}
+
+fn bench_hashmap_from(h: Value) {
+    #[allow(clippy::unit_arg)]
+    black_box(HashMap::<String, String>::from_automerge(&h).unwrap());
 }
 
 fn bench_convert_hashmap(c: &mut Criterion) {
@@ -32,5 +43,49 @@ fn bench_convert_hashmap(c: &mut Criterion) {
     }
 }
 
-criterion_group!(benches, bench_convert_hashmap);
+fn bench_convert_hashmap_to(c: &mut Criterion) {
+    for &limit in &[100, 1000, 10000] {
+        c.bench_function(&format!("convert hashmap with {} entries to", limit), |b| {
+            b.iter_batched(
+                || {
+                    let mut h = HashMap::new();
+                    for i in 0..limit {
+                        h.insert(i.to_string(), i.to_string());
+                    }
+                    h
+                },
+                bench_hashmap_to,
+                criterion::BatchSize::SmallInput,
+            )
+        });
+    }
+}
+
+fn bench_convert_hashmap_from(c: &mut Criterion) {
+    for &limit in &[100, 1000, 10000] {
+        c.bench_function(
+            &format!("convert hashmap with {} entries from", limit),
+            |b| {
+                b.iter_batched(
+                    || {
+                        let mut h = HashMap::new();
+                        for i in 0..limit {
+                            h.insert(i.to_string(), i.to_string());
+                        }
+                        h.to_automerge()
+                    },
+                    bench_hashmap_from,
+                    criterion::BatchSize::SmallInput,
+                )
+            },
+        );
+    }
+}
+
+criterion_group!(
+    benches,
+    bench_convert_hashmap,
+    bench_convert_hashmap_to,
+    bench_convert_hashmap_from
+);
 criterion_main!(benches);
